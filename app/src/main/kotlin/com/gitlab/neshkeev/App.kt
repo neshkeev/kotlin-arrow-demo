676eefb4aab0d827802497e84bff61b4fac70323
Nikita Eshkeev <kastolom@gmail.com>
2019-04-22 01:51:11 +0300

    Extract functor to another file
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- app/src/main/kotlin/com/gitlab/neshkeev/App.kt
+++ app/src/main/kotlin/com/gitlab/neshkeev/App.kt
@@ -1,15 +1,10 @@
 package com.gitlab.neshkeev
 
-import arrow.Kind
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
-import arrow.core.extensions.option.monad.binding
-import arrow.core.fix
-import arrow.extension
 import arrow.higherkind
-import arrow.typeclasses.Functor
-import com.gitlab.neshkeev.parserk.functor.map
+import com.gitlab.neshkeev.extension.parserk.functor.map
 
 /*
 COPIED FROM GENERATED SOURCES
@@ -20,37 +15,23 @@ COPIED FROM GENERATED SOURCES
 //
 //@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
 //inline fun <A> ParserKOf<A>.fix(): ParserK<A> = this as ParserK<A>
-
-data class Result<A>(val res: A, val r: String) {
-    companion object
-}
+//
+//data class Result<A>(val res: A, val r: String) {
+//    companion object
+//}
 
 @higherkind
 data class ParserK<A>(val f: (String) -> Option<Result<A>>): ParserKOf<A> {
     companion object
 }
 
-@extension
-interface ParserKFunctor : Functor<ForParserK> {
-    override fun <A, B> Kind<ForParserK, A>.map(f: (A) -> B): Kind<ForParserK, B> {
-        val (first) = this.fix()
-        val second : (String) -> Option<Result<B>> = {
-            s -> binding {
-                val (a) = first(s)
-                val (res, rest) = a
-                Result(f(res), rest)
-            }
-        }
-
-        return ParserK(second)
-    }
-
-    companion object
+fun anyChr() : ParserK<String> = ParserK { s ->
+    if (s.isEmpty()) None
+    else Some(Result(s[0].toString(), s.substring(1)))
 }
 
 fun main() {
-//    val f: (String) -> Option<Result<String>> = { s -> Some(Result("Hello", s)) }
-//    val a = ParserK(f)
-//    val fix = a.map { it.length }.fix()
-//    println(fix.f("I am fine"))
+    println(anyChr().f("ABC"))
+    println(anyChr().f(""))
+    println(anyChr().map { it.toInt() }.f("1AB"))
 }
